bae3401c80582c9881e24dfda23a6d7a41819f79
forvvard09 <forvvard09@gmail.com>
2022-09-02 02:45:47 +0300

    Done hw02 task3

---
+++ src/main/java/homeworks/lesson04HW/task3/BigCircle.java
@@ -0,0 +1,10 @@
+package homeworks.lesson04HW.task3;
+
+public class BigCircle extends Circle {
+
+    public static final int DEFAULT_RADIUS = 10;
+
+    public BigCircle() {
+        super(DEFAULT_RADIUS);
+    }
+}
