d0cdfc626b53db361163336a6fad5541817527e2
forvvard09 <forvvard09@gmail.com>
2022-09-12 13:59:10 +0300

    Update lessons.

--- src/main/java/homeworks/lesson04HW/task6/Main.java
+++
@@ -1,8 +0,0 @@
-package homeworks.lesson04HW.task6;
-
-public class Main {
-    public static void main(String[] args) {
-        Human human = Human.builder().name("Петр").age(20).weight(80).build();
-        human.getInfoHuman();
-    }
-}
